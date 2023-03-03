f8f8c80ad2abcd2955c1abc1fb8a1d00ae2df789
andreizidoro123 <124374527+andreizidoro123@users.noreply.github.com>
2023-03-03 11:57:58 -0300

    Add files via upload

---
+++ Minha_aplicacao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minha_aplicacao;
+
+/**
+ *
+ * @author Fortec
+ */
+public class Minha_aplicacao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
